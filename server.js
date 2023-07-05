c49d844972e733eda48d44573d230acea8bc9d1b
abdus8samad <abdus8samad@gmail.com>
2023-07-06 00:24:59 +0530

    blablabla

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express'),
+app = express(),
+cors = require('cors'),
+morgan = require('morgan'),
+fs = require('fs'),
+PORT = process.env.PORT || 8080;
+require('dotenv/config');
+
+app.use(cors());
+app.use(morgan('dev'));
+
+let bla = "";
+
+app.get('/', (req, res) =>{
+    if(bla == ""){
+        fs.readFile('blabla.json', 'utf8', (err, data) =>{
+            if(err){
+                return res.status(500).json({error: "Can't read file"});
+            } else {
+                bla = JSON.parse(data);
+            }
+        })
+    }
+    return res.json(bla);
+})
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
